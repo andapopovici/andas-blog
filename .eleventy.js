a1138efd212a2f1415aaaf885dda0be10af456dd
Anda Popovici <anda.popovici@freeagent.com>
2023-12-09 18:25:38 +0000

    Add Netlify CMS, remove Tina

--- .eleventy.js
+++ .eleventy.js
@@ -180,6 +180,8 @@ module.exports = function (eleventyConfig) {
   // We need to copy cached.js only if GA is used
   eleventyConfig.addPassthroughCopy(GA_ID ? "js" : "js/*[!cached].*");
   eleventyConfig.addPassthroughCopy("fonts");
+  // Netlify CMS config
+  eleventyConfig.addPassthroughCopy("src/admin/");
 
   // We need to rebuild upon JS change to update the CSP.
   eleventyConfig.addWatchTarget("./js/");
